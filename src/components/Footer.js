5828d7ca3bf4a665abae6f2eb3ce91b4203301e0
Aditya Singh Rathore <89844174+asrathore257@users.noreply.github.com>
2023-08-23 13:30:38 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
-import React from 'react'
-import heartPng from '../icon/heart.png'
+import React from 'react';
+import heartPng from '../icon/heart.png';
+
 function Footer() {
-  return (
-    <div className='footer'>Made with &nbsp;<span><img style={{'height':'15px'}}src={heartPng}/></span>&nbsp; By ADITYA SINGH RATHORE</div>
-  )
+    return (
+        <div className='footer'>
+            Made with&nbsp;<span><img style={{ 'height': '15px' }} src={heartPng} alt="Heart icon" /></span>&nbsp;By ADITYA SINGH RATHORE
+        </div>
+    );
 }
 
-export default Footer
+export default Footer;
