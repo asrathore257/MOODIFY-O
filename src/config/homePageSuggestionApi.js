b76a34e92f8fda4e0d5e26aa8ecc1df58002ad8d
Aditya Singh Rathore <89844174+asrathore257@users.noreply.github.com>
2023-11-24 19:18:46 +0530

    Update homePageSuggestionApi.js

--- src/config/homePageSuggestionApi.js
+++ src/config/homePageSuggestionApi.js
@@ -1,9 +1,9 @@
 const homePageDateApi={
-    'Upcoming Movies':'http://www.omdbapi.com/?i=tt3896198&apikey=3b572dde',
-    'Popular Movies':'',
-    'Top Rated Movies':'',
-    'Popular TV Shows':'',
-    'Top Rated TV Shows':'',
-    'Trending Carousel':''
+    'Upcoming Movies':'https://api.themoviedb.org/3/movie/upcoming?api_key=f3e5bdb063a2075386810a1acdb4d03e&language=en-US&page=1',
+    'Popular Movies':'https://api.themoviedb.org/3/movie/popular?api_key=f3e5bdb063a2075386810a1acdb4d03e&language=en-US&page=1',
+    'Top Rated Movies':'https://api.themoviedb.org/3/movie/top_rated?api_key=f3e5bdb063a2075386810a1acdb4d03e&language=en-US&page=1',
+    'Popular TV Shows':'https://api.themoviedb.org/3/tv/popular?api_key=f3e5bdb063a2075386810a1acdb4d03e&language=en-US&page=1',
+    'Top Rated TV Shows':'https://api.themoviedb.org/3/tv/top_rated?api_key=f3e5bdb063a2075386810a1acdb4d03e&language=en-US&page=1',
+    'Trending Carousel':'https://api.themoviedb.org/3/trending/all/day?api_key=f3e5bdb063a2075386810a1acdb4d03e'
 }
 export default homePageDateApi
